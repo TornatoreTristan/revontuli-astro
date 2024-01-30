00bc2bc63750a1397721e5feb9ccfacc6ce178bb
AgenceRevontuli <tristan@revontuli.fr>
2024-01-30 11:32:55 +0100

    Add sitemap && robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import partytown from "@astrojs/partytown";
+import minify from "astro-min";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    vue(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  site: "https://www.revontuli.fr",
+  integrations: [vue(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), minify(), sitemap(), robotsTxt()]
 });
