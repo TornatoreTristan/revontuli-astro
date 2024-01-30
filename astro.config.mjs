b47bc029615b1f70d3382321a81f9ee8d6725147
AgenceRevontuli <tristan@revontuli.fr>
2024-01-30 11:04:44 +0100

    fix astro partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-import partytown from "astro-partytown";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
