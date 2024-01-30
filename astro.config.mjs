094debe98800a32145af4dbd12128c053631e41a
AgenceRevontuli <tristan@revontuli.fr>
2024-01-30 11:41:36 +0100

    Fix minify error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import partytown from "@astrojs/partytown";
-import minify from "astro-min";
 import sitemap from "@astrojs/sitemap";
 
 import robotsTxt from "astro-robots-txt";
@@ -9,9 +8,14 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.revontuli.fr",
-  integrations: [vue(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), minify(), sitemap(), robotsTxt()]
+  integrations: [
+    vue(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    sitemap(),
+    robotsTxt(),
+  ],
 });
